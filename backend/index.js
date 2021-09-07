ce85f5b8a964d1a19c44b0a3d128862b8af4c288
Jose Miguel <josyto7@gmail.com>
2021-09-07 19:36:29 +0200

    First mongoose example
    Co-authored-by: Pablo Castellanos <pablocdev@users.noreply.github.com>

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,24 @@
 require("dotenv").config();
 const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
 const port = 4000;
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${process.env.PORT}`);
-});
+(async () => {
+  await mongoose.connect(process.env.MONGODB_URL);
+  console.log("DB connected!");
+
+  //Mongoose Models
+  const Product = mongoose.model("Product", { name: String, price: Number });
+
+  const sandia = new Product({ name: "Sandía", price: 1 });
+
+  await sandia.save();
+  console.log("Sandía guardada");
+
+  app.listen(port, () => {
+    console.log(
+      `Example app listening at http://localhost:${process.env.PORT}`
+    );
+  });
+})();
